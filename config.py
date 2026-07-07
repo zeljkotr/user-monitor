df640f759d30aa70f77521d7f07586d7d0786c3d
zeljkotr <zeljko.tripcevski@aol.com>
2026-07-07 13:48:29 +0200

    povecani pragovi resursa

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Pragovi za upozorenja
-CPU_PRAG = 70
-RAM_PRAG = 50
-MREZA_PRAG_MB = 5    # MB/s — kad predje ovo, mreza je zasicena
+CPU_PRAG = 90
+RAM_PRAG = 60
+MREZA_PRAG_MB = 5100    # MB/s — kad predje ovo, mreza je zasicena
 CHROME_TABOVI_PRAG = 15  # broj Chrome procesa
 
 # Interval provere u sekundama
